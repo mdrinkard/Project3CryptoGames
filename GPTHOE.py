7a97b8c8535bf49b02f72518860deeae2ae2937a
Michael <MichaelDavidDrinkard@gmail.com>
2024-02-22 17:37:57 -0800

    Undid some errors I created

--- GPTHOE.py
+++ GPTHOE.py
@@ -1,5 +1,6 @@
 import random
 import streamlit as st
+from PIL import Image
 
 # Deck of Cards
 suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
@@ -7,33 +8,32 @@ ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 
 deck = [{'rank': rank, 'suit': suit} for suit in suits for rank in ranks]
 
 # Function to deal a card
-def deal_card():
+def dealCard():
     card = random.choice(deck)
     deck.remove(card)
     return card
 
 # Function to calculate the total value of a hand
-def calculate_total(hand):
-    total = 0
+def total(hand):
+    total_val = 0
     num_aces = 0
     for card in hand:
         if card['rank'] in ['Jack', 'Queen', 'King']:
-            total += 10
+            total_val += 10
         elif card['rank'] == 'Ace':
             num_aces += 1
-            total += 11
+            total_val += 11
         else:
-            total += int(card['rank'])
-    while total > 21 and num_aces:
-        total -= 10
+            total_val += int(card['rank'])
+
+    while total_val > 21 and num_aces > 0:
+        total_val -= 10
         num_aces -= 1
-    return total
+
+    return total_val
 
 # Function to display a hand
 def display_hand(hand, hide_first_card=False):
-    """
-    Function to display a hand.
-    """
     for card in hand:
         if hide_first_card and card == hand[0]:
             st.image('Project3CryptoGames/Resources/back.png', width=100)
@@ -41,51 +41,75 @@ def display_hand(hand, hide_first_card=False):
             image_path = f"Project3CryptoGames/Resources/{card['rank']}_{card['suit']}.png"
             st.image(image_path, width=100)
 
-# Streamlit app title
-st.title("Blackjack")
+# Initial setup
+dealerHand = []
+playerHand = []
 
-# Initialize game state variables
-player_hand = []
-dealer_hand = []
-
-# Deal initial cards
+# Initial dealing
 for _ in range(2):
-    player_hand.append(deal_card())
-    dealer_hand.append(deal_card())
+    dealerHand.append(dealCard())
+    playerHand.append(dealCard())
 
-# Main game loop
-player_total = calculate_total(player_hand)
-dealer_total = calculate_total(dealer_hand)
+# Streamlit App
+st.title('Blackjack')
 
-st.write("Dealer's Hand:")
-display_hand(dealer_hand, hide_first_card=True)
-st.write(f"Dealer's Total: {dealer_total}")
+st.write('Dealer\'s Hand:')
+display_hand(dealerHand, hide_first_card=True)
 
-st.write("Your Hand:")
-display_hand(player_hand)
-st.write(f"Your Total: {player_total}")
+st.write('Your Hand:')
+display_hand(playerHand)
 
-while st.button('Hit'):
-    player_hand.append(deal_card())
-    player_total = calculate_total(player_hand)
-    st.write("Your Hand:")
-    display_hand(player_hand)
-    st.write(f"Your Total: {player_total}")
-    if player_total > 21:
-        st.write("You bust! Dealer wins.")
-        break
+# Game logic
+player_total = total(playerHand)
+dealer_total = total(dealerHand)
 
-if player_total <= 21:
-    while dealer_total < 17:
-        dealer_hand.append(deal_card())
-        dealer_total = calculate_total(dealer_hand)
-    st.write("Dealer's Hand:")
-    display_hand(dealer_hand)
-    st.write(f"Dealer's Total: {dealer_total}")
+if player_total == 21:
+    st.write('Blackjack! You win!')
+elif player_total > 21:
+    st.write('You bust! Dealer wins!')
+else:
+    st.write('Select your move:')
+    move = st.radio('', ('Hit', 'Stand'))
+    while move == 'Hit':
+        if move == 'Hit':
+            playerHand.append(dealCard())
+            st.write('Your Hand:')
+            display_hand(playerHand)
 
-    if dealer_total > 21 or player_total > dealer_total:
-        st.write("You win!")
-    elif player_total < dealer_total:
-        st.write("Dealer wins.")
-    else:
-        st.write("It's a tie!")
+            player_total = total(playerHand)
+            if player_total > 21:
+                st.write('You bust! Dealer wins!')
+                break
+            elif player_total == 21:
+                st.write('Blackjack! You win!')
+                break
+            else:
+                st.write('Select your move:')
+                move = st.radio('', ('Hit', 'Stand'))
+        else:
+            break
+    if move == 'Stand':  # Stand
+        st.write('Your final hand:')
+        display_hand(playerHand)
+
+        st.write('Dealer\'s turn:')
+        st.write('Dealer\'s Hand:')
+        display_hand(dealerHand)
+
+        # Dealer's turn
+        while total(dealerHand) < 17:
+            dealerHand.append(dealCard())
+            st.write('Dealer hits:')
+            st.write('Dealer\'s Hand:')
+            display_hand(dealerHand)
+
+        dealer_total = total(dealerHand)
+
+        if dealer_total > 21:
+            st.write('Dealer busts! You win!')
+        elif dealer_total > player_total:
+            st.write('Dealer wins!')
+        elif dealer_total < player_total:
+            st.write('You win!')
+        else:
+            st.write('It\'s a tie!')
